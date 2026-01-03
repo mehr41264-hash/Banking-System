eea2aa6af2cf533b50af52e75782bc9598d3f943
Mehr Ali <mehr41264@gmail.com>
2026-01-03 19:01:53 +0500

    Adding a new feature to deposit the amount

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print('Welcome to the bank "XYZ" ')
 #Creating the balance check function for the user for the option of menu
 balance = 0
 def balance_check(balance):
-    print(f"Your balance is {balance}")
+    print(f"Your balance is {balance}$")
 
 #Initialization of user's input for Signup/Login
 print("Select an option to continue" "\n"
@@ -27,18 +27,21 @@ while True:
 #Creating the menu function for the  users to initialize the menu settings
 def user_menu_call(balance):
     print("Select an option to continue" "\n"
-    "1. Deposit Ammount" "\n"
-    "2. Withdraw Ammount" "\n"
+    "1. Deposit Amount" "\n"
+    "2. Withdraw Amount" "\n"
     "3. Check Balance" "\n"
     "4. Share Balance" "\n"
     "5. Exit")
     while True:
         user_menu = input("Enter the option: ").lower().strip()
-        if user_menu not in ("depositammount", "withdrawammount", "checkbalance", "sharebalance", "exit"):
+        if user_menu not in ("depositamount", "withdrawamount", "checkbalance", "sharebalance", "exit"):
             print("Invalid Action, try again")
             continue
         if user_menu == "checkbalance":
             balance_check(balance)
+            continue
+        elif user_menu == "depositamount":
+            balance = deposit(logged_username, balance)
             continue
         elif user_menu == "exit":
             print("Thanks for using XYZ Bank")
@@ -52,19 +55,19 @@ with open("/home/mehr-ali/Documents/Banking System/usersdata.txt", "r") as file:
     for data in file:
         data = data.strip()
         if data:
-            username, password, fullname, pin, initial_deposit, unique_id = data.split(",")
+            username, password, fullname, pin, balance, unique_id = data.split(",")
             pin = int(pin)
-            initial_deposit = int(initial_deposit)
+            balance = int(balance)
             unique_id = int(unique_id)
-            userdata.append((username, password, fullname, pin, initial_deposit, unique_id))
+            userdata.append((username, password, fullname, pin, balance, unique_id))
 
 #Creating the signup function for the new users
 class Signup():
-    def __init__(self, username, password, fullname, pin, initial_deposit, unique_id):
+    def __init__(self, username, password, fullname, pin, balance, unique_id):
         self.username = username
         self.password = password
         self.pin = pin
-        self.initial_deposit = initial_deposit
+        self.balance = balance
         self.unique_id = unique_id
         self.fullname = fullname
 
@@ -105,25 +108,25 @@ def signup():
     while True:
         print("You have to deposit the ammount for the creation of your account" "\n"
             "Minimum deposit is 10$")
-        initial_deposit = int(input("Enter the ammount you want to enter: "))
-        if initial_deposit < 10:
-            print("Minimum Deposit is 10$")
+        balance = int(input("Enter the ammount you want to enter: "))
+        if balance < 10:
+            print("Minimum amount is 10$")
             continue
         break
     #Adding the input into the file
     with open("/home/mehr-ali/Documents/Banking System/usersdata.txt", "a") as file:
-        file.write(f"{username},{password},{fullname},{pin},{initial_deposit},{unique_id}\n")
+        file.write(f"{username},{password},{fullname},{pin},{balance},{unique_id}\n")
 
     #Printing the message after successful signup
     print("Account was created successfully")
     #Now calling the class as everything is defined now completly.
-    signup_user = Signup(username, password, fullname, pin, initial_deposit, unique_id)
+    signup_user = Signup(username, password, fullname, pin, balance, unique_id)
     #Showing the input to the user after their account creation
     print(f"Your Username is {signup_user.username}")
     print(f"Your Password is {signup_user.password}")
     print(f"Your Full Name is {signup_user.fullname}")
     print(f"Your PIN is {signup_user.pin}")
-    print(f"Your Initial Deposit is {signup_user.initial_deposit}$")
+    print(f"Your Initial balance is {signup_user.balance}$")
     print(f"Your Banking ID is {signup_user.unique_id}")
     return signup_user
 
@@ -144,7 +147,7 @@ def login():
             print("Password must not be empty")
             continue
         for user_data in userdata:
-            user, password, fullname, pin, initial_deposit, unique_id = user_data
+            user, password, fullname, pin, balance, unique_id = user_data
             if user == username_login:
                 if password == password_login:
                     print("Login Successfull")
@@ -153,17 +156,45 @@ def login():
                     print("Password didn't matched")
                 break
 
+#Creating the balance function to balance ammount in the user's account
+def deposit(logged_user, balance):
+    while True:
+        amount_input = input("Enter the amount you want to enter in your account: ").strip()
+        if amount_input == "":
+            print("Amount can't be empty, try again")
+            continue
+        if not amount_input.isdigit():
+            print("Enter the amount in digits")
+            continue
+
+        amount = int(amount_input)
+
+        if amount <= 0:
+            print("You are Entering wrong amount, try again")
+            continue
+        break
+    balance += amount
+    print(f"You entered the amount of {amount}$.Total balance is now {balance}$")
+    with open("/home/mehr-ali/Documents/Banking System/usersdata.txt", "w") as file:
+        for i, user in enumerate(userdata):
+            u, p, f, pin, dep, uid = user
+            if u == logged_user:
+                dep = balance
+                userdata[i] = (u, p, f, pin, dep, uid)
+            file.write(f"{u},{p},{f},{pin},{dep},{uid}\n")
+        return balance
 
 #Calling the menu function for the signup option
 if user == "signup":
     new_user = signup()
-    balance = new_user.initial_deposit
+    balance = new_user.balance
     user_menu_call(balance)
 
-#Loading the user's deposit ammount into the list also calling the function
+#Loading the user's balance ammount into the list also calling the function
 elif user == "login":
     logged_user = login()
     if logged_user:
+        logged_username = logged_user[0]
         balance = logged_user[4]
         user_menu_call(balance)
 
